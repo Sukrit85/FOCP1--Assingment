eb42e142e79b6b847dfa4b627193516b9bfab0d6
Sukrit85 <sukrit25csu237@ncuindia.edu>
2025-11-10 13:13:48 +0530

    Create Q9.c

---
+++ Q9.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main (){
+    int a , flag=0 ;
+    printf("Enter total number of scores : ");
+    scanf("%d",&a);
+    int score[a];
+    for(int i = 0 ; i<a ; i+=1){
+        printf("Enter score %d : ",i+1);
+        scanf("%d",&score[i]);
+    }
+    for(int i = 0 ; i<a ; i+=1){
+        if(score[i]==99){
+            printf("First occurence of score 99 is at the score %d ie array = score[%d]",i+1,i);
+            flag=1;
+            break;
+        }
+    }
+    if(flag==0){
+        printf("Score 99 not found");
+    }
+    return 0;
+}
